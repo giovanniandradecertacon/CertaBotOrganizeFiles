7e91612421c502e630dbfb08acd036bfc7c311b7
giovanniandradecertacon <giovanni.andrade@certacon.com.br>
2023-05-08 15:23:57 -0300

    feat: entity created to save and manipulate File

---
+++ src/main/java/entity/FilesEntity.java
@@ -0,0 +1,30 @@
+package entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.util.UUID;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class FilesEntity {
+    @Id
+    @GenericGenerator(name = "UUIDGenerator", strategy = "uuid2")
+    @GeneratedValue(generator = "UUIDGenerator")
+    @Column(name = "file_id")
+    @JsonProperty(value = "file_id")
+    private UUID id;
+
+
+}
