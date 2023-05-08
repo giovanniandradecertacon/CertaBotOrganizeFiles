053cce4edfb51a8ce02a7fb9a966b636818de9e2
giovanniandradecertacon <giovanni.andrade@certacon.com.br>
2023-05-08 11:09:27 -0300

    first commit

---
+++ src/main/java/br/com/certacon/certabotorganizefiles/CertaBotOrganizeFilesApplication.java
@@ -0,0 +1,13 @@
+package br.com.certacon.certabotorganizefiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertaBotOrganizeFilesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CertaBotOrganizeFilesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/certacon/certabotorganizefiles/CertaBotOrganizeFilesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.certacon.certabotorganizefiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertaBotOrganizeFilesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
