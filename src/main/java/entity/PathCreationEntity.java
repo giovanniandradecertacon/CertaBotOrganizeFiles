672a40dd57825f284a395f11e718a779697960ce
giovanniandradecertacon <giovanni.andrade@certacon.com.br>
2023-05-08 15:26:09 -0300

    feat: entity created to save path informations

---
+++ src/main/java/entity/PathCreationEntity.java
@@ -0,0 +1,42 @@
+package entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.util.UUID;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class PathCreationEntity {
+    @Id
+    @GenericGenerator(name = "UUIDGenerator", strategy = "uuid2")
+    @GeneratedValue(generator = "UUIDGenerator")
+    @Column(name = "path_id")
+    @JsonProperty(value = "path_id")
+    private UUID id;
+    @Column(name = "raiz")
+    private String root;
+
+    @Column(name = "funcao_folder")
+    private String function;
+
+    @Column(name = "ip_servidor")
+    private String ipServer;
+
+    @Column(name = "cnpj")
+    private String cnpj;
+
+    @Column(name = "year")
+    private String year;
+}
