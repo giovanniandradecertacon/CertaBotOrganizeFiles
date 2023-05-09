4204560c2e091c65acbe7a5527619a4fdb7b37a0
giovanniandradecertacon <giovanni.andrade@certacon.com.br>
2023-05-09 17:40:03 -0300

    feat: creation of Schedule to move and Organize files

---
+++ src/main/java/br/com/certacon/certabotorganizefiles/schedule/MoveAndOrganizeFilesSchedule.java
@@ -0,0 +1,38 @@
+package br.com.certacon.certabotorganizefiles.schedule;
+
+import br.com.certacon.certabotorganizefiles.component.MoveFilesComponent;
+import br.com.certacon.certabotorganizefiles.entity.FilesEntity;
+import br.com.certacon.certabotorganizefiles.repository.FilesRepository;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.List;
+
+@Service
+public class MoveAndOrganizeFilesSchedule {
+    private final MoveFilesComponent moveFilesComponent;
+
+    private final FilesRepository filesRepository;
+
+    public MoveAndOrganizeFilesSchedule(MoveFilesComponent moveFilesComponent, FilesRepository filesRepository) {
+        this.moveFilesComponent = moveFilesComponent;
+        this.filesRepository = filesRepository;
+    }
+
+    @Scheduled(fixedRate = 30000)
+    public boolean moveAndOrganizeScheduled() {
+        List<FilesEntity> fileList = filesRepository.findAll();
+        if (!fileList.isEmpty()) {
+            fileList.forEach(files -> {
+                try {
+                    files = moveFilesComponent.moveFiles(new File(files.getFilePath()));
+                } catch (FileNotFoundException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        }
+        return true;
+    }
+}
