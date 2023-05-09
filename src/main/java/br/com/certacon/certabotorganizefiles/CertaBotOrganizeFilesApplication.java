d690f4a0238a051b4579bae7f37a674ca0577552
giovanniandradecertacon <giovanni.andrade@certacon.com.br>
2023-05-09 17:39:06 -0300

    chore: annotation for schedule added

--- src/main/java/br/com/certacon/certabotorganizefiles/CertaBotOrganizeFilesApplication.java
+++ src/main/java/br/com/certacon/certabotorganizefiles/CertaBotOrganizeFilesApplication.java
@@ -2,8 +2,10 @@ package br.com.certacon.certabotorganizefiles;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class CertaBotOrganizeFilesApplication {
 
     public static void main(String[] args) {
