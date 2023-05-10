ad781fadafca0dfca78859ece02b5305ba658670
giovanniandradecertacon <giovanni.andrade@certacon.com.br>
2023-05-10 17:19:06 -0300

    chore: created methods and renamed class

--- src/main/java/br/com/certacon/certabotorganizefiles/helper/UnzipAndZipFilesHelper.java
+++ src/main/java/br/com/certacon/certabotorganizefiles/helper/UnzipAndZipFilesHelper.java
@@ -1,5 +1,7 @@
 package br.com.certacon.certabotorganizefiles.helper;
 
+import br.com.certacon.certabotorganizefiles.entity.PathCreationEntity;
+import br.com.certacon.certabotorganizefiles.utils.FileFoldersFunction;
 import br.com.certacon.certabotorganizefiles.utils.FileStatus;
 import org.apache.commons.compress.archivers.ArchiveEntry;
 import org.apache.commons.compress.archivers.ArchiveInputStream;
@@ -11,9 +13,30 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.util.UUID;
 
 @Component
-public class UnzipFilesHelper {
+public class UnzipAndZipFilesHelper {
+    public FileStatus directoryCreator(PathCreationEntity entityForCreation) {
+        Path uuidPath = Path.of("D:"
+                + File.separator + FileFoldersFunction.ORGANIZAR
+                + File.separator + entityForCreation.getIpServer()
+                + File.separator + entityForCreation.getCnpj()
+                + File.separator + entityForCreation.getYear()
+                + File.separator + UUID.randomUUID());
+        Path compactedDir = Path.of(uuidPath
+                + File.separator + FileFoldersFunction.COMPACTADOS);
+        Path descomPactedDir = Path.of(uuidPath
+                + File.separator + FileFoldersFunction.DESCOMPACTADOS);
+        if (!uuidPath.toFile().exists()) uuidPath.toFile().mkdirs();
+        if (!compactedDir.toFile().exists()) compactedDir.toFile().mkdirs();
+        if (!descomPactedDir.toFile().exists()) descomPactedDir.toFile().mkdirs();
+
+        return FileStatus.CREATEDDIRS;
+    }
+
+
     public FileStatus unzipFile(File zipFile, File destinyDirectory) {
         try (ArchiveInputStream entrada = new ZipArchiveInputStream(new FileInputStream(zipFile))) {
             if (!destinyDirectory.exists()) {
