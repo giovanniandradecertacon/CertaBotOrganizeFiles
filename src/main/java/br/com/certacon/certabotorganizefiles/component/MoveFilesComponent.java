0a80d78d40b5875d16e4ff5306f5125b8c1bdb1b
giovanniandradecertacon <giovanni.andrade@certacon.com.br>
2023-05-10 09:28:58 -0300

    chore: created entity for each file

--- src/main/java/br/com/certacon/certabotorganizefiles/component/MoveFilesComponent.java
+++ src/main/java/br/com/certacon/certabotorganizefiles/component/MoveFilesComponent.java
@@ -3,6 +3,7 @@ package br.com.certacon.certabotorganizefiles.component;
 import br.com.certacon.certabotorganizefiles.entity.FilesEntity;
 import br.com.certacon.certabotorganizefiles.entity.PathCreationEntity;
 import br.com.certacon.certabotorganizefiles.helper.MoveFilesHelper;
+import br.com.certacon.certabotorganizefiles.repository.FilesRepository;
 import br.com.certacon.certabotorganizefiles.utils.FileStatus;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.compress.utils.FileNameUtils;
@@ -18,26 +19,36 @@ import static java.nio.file.StandardCopyOption.ATOMIC_MOVE;
 @Slf4j
 public class MoveFilesComponent {
     private final MoveFilesHelper helper;
+    private final FilesRepository filesRepository;
 
-    public MoveFilesComponent(MoveFilesHelper helper) {
+    public MoveFilesComponent(MoveFilesHelper helper, FilesRepository filesRepository) {
         this.helper = helper;
+        this.filesRepository = filesRepository;
     }
 
     public FilesEntity moveFiles(File loadDirectory) throws FileNotFoundException {
         if (loadDirectory.exists()) {
+            FilesEntity saveFile = null;
             PathCreationEntity pathComponents = helper.pathSplitter(loadDirectory);
             pathComponents.setPath(loadDirectory.getPath());
             File[] listFiles = loadDirectory.listFiles();
             for (int i = 0; i < listFiles.length; i++) {
+                saveFile = FilesEntity.builder()
+                        .filePath(listFiles[i].getPath())
+                        .fileName(listFiles[i].getName())
+                        .status(FileStatus.CREATED)
+                        .build();
+                filesRepository.save(saveFile);
                 if (FileNameUtils.getExtension(listFiles[i].getName()).equals("txt")
                         || FileNameUtils.getExtension(listFiles[i].getName()).equals("zip")
+                        || FileNameUtils.getExtension(listFiles[i].getName()).equals("rar")
                         || FileNameUtils.getExtension(listFiles[i].getName()).equals("xml")) {
                     Path organizedEFDPath = helper.pathCreatorForToOrganize(pathComponents);
                     FileStatus fileStatus = helper.moveFile(listFiles[i], Path.of(organizedEFDPath + File.separator + listFiles[i].getName()), ATOMIC_MOVE);
-                    log.info(String.valueOf(fileStatus));
+                    saveFile.setStatus(fileStatus);
                 }
             }
-            return FilesEntity.builder().build();
+            return saveFile;
         } else {
             throw new FileNotFoundException("Diretório não existente");
         }
