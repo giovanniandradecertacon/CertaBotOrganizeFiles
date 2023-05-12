e1784eabe5928854f2b2180def128fda3ac83f01
giovanniandradecertacon <giovanni.andrade@certacon.com.br>
2023-05-12 13:41:03 -0300

    chore: implemented unzip method

--- src/main/java/br/com/certacon/certabotorganizefiles/schedule/MoveAndOrganizeFilesSchedule.java
+++ src/main/java/br/com/certacon/certabotorganizefiles/schedule/MoveAndOrganizeFilesSchedule.java
@@ -1,6 +1,7 @@
 package br.com.certacon.certabotorganizefiles.schedule;
 
 import br.com.certacon.certabotorganizefiles.component.MoveFilesComponent;
+import br.com.certacon.certabotorganizefiles.component.UnzipFilesComponent;
 import br.com.certacon.certabotorganizefiles.entity.FilesEntity;
 import br.com.certacon.certabotorganizefiles.repository.FilesRepository;
 import br.com.certacon.certabotorganizefiles.utils.FileStatus;
@@ -10,16 +11,19 @@ import org.springframework.stereotype.Service;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.List;
 
 @Service
 public class MoveAndOrganizeFilesSchedule {
     private final MoveFilesComponent moveFilesComponent;
+    private final UnzipFilesComponent unzipFilesComponent;
 
     private final FilesRepository filesRepository;
 
-    public MoveAndOrganizeFilesSchedule(MoveFilesComponent moveFilesComponent, FilesRepository filesRepository) {
+    public MoveAndOrganizeFilesSchedule(MoveFilesComponent moveFilesComponent, UnzipFilesComponent unzipFilesComponent, FilesRepository filesRepository) {
         this.moveFilesComponent = moveFilesComponent;
+        this.unzipFilesComponent = unzipFilesComponent;
         this.filesRepository = filesRepository;
     }
 
@@ -32,11 +36,16 @@ public class MoveAndOrganizeFilesSchedule {
                     if (files.getStatus() == FileStatus.CREATED
                             || files.getStatus() == FileStatus.UPDATED) {
                         files = moveFilesComponent.moveFiles(new File(files.getFilePath()));
+                        filesRepository.save(files);
                         if (files.getStatus() == FileStatus.MOVED && FileNameUtils.getExtension(files.getFileName()).equals("rar")
                                 || FileNameUtils.getExtension(files.getFileName()).equals("zip")) {
+                            files = unzipFilesComponent.MoveAndUnzip(new File(files.getFilePath()));
+                            filesRepository.save(files);
                         }
                     }
                 } catch (FileNotFoundException e) {
+                    throw new RuntimeException(e);
+                } catch (IOException e) {
                     throw new RuntimeException(e);
                 }
             });
