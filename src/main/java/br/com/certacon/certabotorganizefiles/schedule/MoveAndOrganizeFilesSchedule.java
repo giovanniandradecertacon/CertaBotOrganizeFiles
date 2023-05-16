c1c9fa53e128a3d9c92e444bf3642dc9a055c375
giovanniandradecertacon <giovanni.andrade@certacon.com.br>
2023-05-16 17:58:34 -0300

    fix: code improvement

--- src/main/java/br/com/certacon/certabotorganizefiles/schedule/MoveAndOrganizeFilesSchedule.java
+++ src/main/java/br/com/certacon/certabotorganizefiles/schedule/MoveAndOrganizeFilesSchedule.java
@@ -1,10 +1,8 @@
 package br.com.certacon.certabotorganizefiles.schedule;
 
-import br.com.certacon.certabotorganizefiles.component.MoveFilesComponent;
-import br.com.certacon.certabotorganizefiles.component.OrganizeComponent;
-import br.com.certacon.certabotorganizefiles.component.UnzipFilesComponent;
-import br.com.certacon.certabotorganizefiles.component.ZipFilesComponent;
+import br.com.certacon.certabotorganizefiles.component.*;
 import br.com.certacon.certabotorganizefiles.entity.FilesEntity;
+import br.com.certacon.certabotorganizefiles.entity.UserFilesEntity;
 import br.com.certacon.certabotorganizefiles.repository.FilesRepository;
 import br.com.certacon.certabotorganizefiles.utils.FileStatus;
 import org.apache.commons.compress.utils.FileNameUtils;
@@ -14,6 +12,7 @@ import org.springframework.stereotype.Service;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.file.Path;
 import java.util.List;
 
 @Service
@@ -22,14 +21,16 @@ public class MoveAndOrganizeFilesSchedule {
     private final UnzipFilesComponent unzipFilesComponent;
     private final OrganizeComponent organizeComponent;
     private final ZipFilesComponent zipFilesComponent;
+    private final SaveFilesForRestComponent saveFilesForRestComponent;
 
     private final FilesRepository filesRepository;
 
-    public MoveAndOrganizeFilesSchedule(MoveFilesComponent moveFilesComponent, UnzipFilesComponent unzipFilesComponent, OrganizeComponent organizeComponent, ZipFilesComponent zipFilesComponent, FilesRepository filesRepository) {
+    public MoveAndOrganizeFilesSchedule(MoveFilesComponent moveFilesComponent, UnzipFilesComponent unzipFilesComponent, OrganizeComponent organizeComponent, ZipFilesComponent zipFilesComponent, SaveFilesForRestComponent saveFilesForRestComponent, FilesRepository filesRepository) {
         this.moveFilesComponent = moveFilesComponent;
         this.unzipFilesComponent = unzipFilesComponent;
         this.organizeComponent = organizeComponent;
         this.zipFilesComponent = zipFilesComponent;
+        this.saveFilesForRestComponent = saveFilesForRestComponent;
         this.filesRepository = filesRepository;
     }
 
@@ -40,24 +41,31 @@ public class MoveAndOrganizeFilesSchedule {
             fileList.forEach(files -> {
                 try {
                     if (files.getStatus() == FileStatus.CREATED
-                            || files.getStatus() == FileStatus.UPDATED) {
-                        files = moveFilesComponent.moveFiles(new File(files.getFilePath()));
+                            || files.getStatus() == FileStatus.UPDATED && new File(files.getFilePath()).isDirectory()) {
+                        files = moveFilesComponent.moveFiles(files);
+                        filesRepository.save(files);
                         if (files.getStatus() == FileStatus.MOVED && FileNameUtils.getExtension(files.getFileName()).equals("rar")
                                 || FileNameUtils.getExtension(files.getFileName()).equals("zip")) {
-                            files = unzipFilesComponent.MoveAndUnzip(new File(files.getFilePath()));
+                            files = unzipFilesComponent.MoveAndUnzip(files);
                             filesRepository.save(files);
                             if (files.getStatus() == FileStatus.EXTRACTED) {
                                 FileStatus fileStatus = organizeComponent.organizeFilesForZipping(new File(files.getFilePath()), files.getId());
                                 files.setStatus(fileStatus);
+                                filesRepository.save(files);
                             }
                             if (files.getStatus() == FileStatus.READY) {
-                                FileStatus fileStatus = zipFilesComponent.zipFilesForUpload(new File(files.getFilePath()));
+                                Path filePath = zipFilesComponent.zipFilesForUpload(new File(files.getFilePath()));
+                                files.setStatus(FileStatus.ZIPPED);
+                                files.setFilePath(filePath.toString());
+                                filesRepository.save(files);
+                            }
+                            if (files.getStatus() == FileStatus.ZIPPED) {
+                                UserFilesEntity userForRest = saveFilesForRestComponent.saveFilesForRest(files);
+                                userForRest.getFileName();
                             }
                         }
 
                     }
-
-
                 } catch (FileNotFoundException e) {
                     files.setStatus(FileStatus.ERROR);
                     filesRepository.save(files);
