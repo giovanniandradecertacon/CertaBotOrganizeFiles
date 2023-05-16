1d776ba00a5cddb91129baf25e411a671105adb8
giovanniandradecertacon <giovanni.andrade@certacon.com.br>
2023-05-16 10:50:51 -0300

    fix: implemented status setters

--- src/main/java/br/com/certacon/certabotorganizefiles/schedule/MoveAndOrganizeFilesSchedule.java
+++ src/main/java/br/com/certacon/certabotorganizefiles/schedule/MoveAndOrganizeFilesSchedule.java
@@ -3,6 +3,7 @@ package br.com.certacon.certabotorganizefiles.schedule;
 import br.com.certacon.certabotorganizefiles.component.MoveFilesComponent;
 import br.com.certacon.certabotorganizefiles.component.OrganizeComponent;
 import br.com.certacon.certabotorganizefiles.component.UnzipFilesComponent;
+import br.com.certacon.certabotorganizefiles.component.ZipFilesComponent;
 import br.com.certacon.certabotorganizefiles.entity.FilesEntity;
 import br.com.certacon.certabotorganizefiles.repository.FilesRepository;
 import br.com.certacon.certabotorganizefiles.utils.FileStatus;
@@ -20,13 +21,15 @@ public class MoveAndOrganizeFilesSchedule {
     private final MoveFilesComponent moveFilesComponent;
     private final UnzipFilesComponent unzipFilesComponent;
     private final OrganizeComponent organizeComponent;
+    private final ZipFilesComponent zipFilesComponent;
 
     private final FilesRepository filesRepository;
 
-    public MoveAndOrganizeFilesSchedule(MoveFilesComponent moveFilesComponent, UnzipFilesComponent unzipFilesComponent, OrganizeComponent organizeComponent, FilesRepository filesRepository) {
+    public MoveAndOrganizeFilesSchedule(MoveFilesComponent moveFilesComponent, UnzipFilesComponent unzipFilesComponent, OrganizeComponent organizeComponent, ZipFilesComponent zipFilesComponent, FilesRepository filesRepository) {
         this.moveFilesComponent = moveFilesComponent;
         this.unzipFilesComponent = unzipFilesComponent;
         this.organizeComponent = organizeComponent;
+        this.zipFilesComponent = zipFilesComponent;
         this.filesRepository = filesRepository;
     }
 
@@ -45,16 +48,27 @@ public class MoveAndOrganizeFilesSchedule {
                             filesRepository.save(files);
                             if (files.getStatus() == FileStatus.EXTRACTED) {
                                 FileStatus fileStatus = organizeComponent.organizeFilesForZipping(new File(files.getFilePath()), files.getId());
-                                System.out.println(fileStatus);
+                                files.setStatus(fileStatus);
+                            }
+                            if (files.getStatus() == FileStatus.READY) {
+                                FileStatus fileStatus = zipFilesComponent.zipFilesForUpload(new File(files.getFilePath()));
                             }
                         }
+
                     }
+
+
                 } catch (FileNotFoundException e) {
+                    files.setStatus(FileStatus.ERROR);
+                    filesRepository.save(files);
                     throw new RuntimeException(e);
                 } catch (IOException e) {
+                    files.setStatus(FileStatus.ERROR);
+                    filesRepository.save(files);
                     throw new RuntimeException(e);
                 }
             });
+
             return Boolean.TRUE;
         }
         return Boolean.FALSE;
