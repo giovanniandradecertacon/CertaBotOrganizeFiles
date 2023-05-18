a0e84f59528b91fddc7f28092a8a3c542a840225
giovanniandradecertacon <giovanni.andrade@certacon.com.br>
2023-05-18 09:58:14 -0300

    chore: code improvement

--- src/main/java/br/com/certacon/certabotorganizefiles/component/ZipFilesComponent.java
+++ src/main/java/br/com/certacon/certabotorganizefiles/component/ZipFilesComponent.java
@@ -3,7 +3,6 @@ package br.com.certacon.certabotorganizefiles.component;
 import br.com.certacon.certabotorganizefiles.entity.PathCreationEntity;
 import br.com.certacon.certabotorganizefiles.helper.UnzipAndZipFilesHelper;
 import br.com.certacon.certabotorganizefiles.utils.FileFoldersFunction;
-import br.com.certacon.certabotorganizefiles.utils.FileStatus;
 import br.com.certacon.certabotorganizefiles.utils.FileType;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.tomcat.util.http.fileupload.FileUtils;
@@ -12,6 +11,8 @@ import org.springframework.stereotype.Component;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 @Slf4j
@@ -22,36 +23,38 @@ public class ZipFilesComponent {
         this.helper = helper;
     }
 
-    public Path zipFilesForUpload(File uuidDirectory) throws IOException {
+    public List<File> zipFilesForUpload(File uuidDirectory) throws IOException {
         if (uuidDirectory.exists()) {
-            File destiny = null;
+            List<File> destiny = new ArrayList<>();
             File[] listFiles = uuidDirectory.listFiles();
-            FileStatus status = null;
             for (int i = 0; i < listFiles.length; i++) {
                 if (listFiles[i].getName().startsWith("EFDS-")) {
                     PathCreationEntity creationEntity = helper.pathSplitter(uuidDirectory);
-                    destiny = new File(creationEntity.getRoot() +
+                    File destinyEfd = new File(creationEntity.getRoot() +
                             File.separator + FileFoldersFunction.ORGANIZADOS +
                             File.separator + creationEntity.getIpServer() +
                             File.separator + creationEntity.getCnpj() +
                             File.separator + creationEntity.getYear() +
                             File.separator + FileType.EFDPadrao);
-                    status = helper.zipFiles(listFiles[i], destiny);
-                    log.info(status.name());
+                    Path efdPath = helper.zipFiles(listFiles[i], destinyEfd);
+
+                    destiny.add(efdPath.toFile());
+
                 } else if (listFiles[i].getName().startsWith("XMLS-")) {
                     PathCreationEntity creationEntity = helper.pathSplitter(uuidDirectory);
-                    destiny = new File(creationEntity.getRoot() +
+                    File destinyNFe = new File(creationEntity.getRoot() +
                             File.separator + FileFoldersFunction.ORGANIZADOS +
                             File.separator + creationEntity.getIpServer() +
                             File.separator + creationEntity.getCnpj() +
                             File.separator + creationEntity.getYear() +
                             File.separator + FileType.NFe);
-                    status = helper.zipFiles(listFiles[i], destiny);
-                    log.info(status.name());
+                    Path nfePath = helper.zipFiles(listFiles[i], destinyNFe);
+
+                    destiny.add(nfePath.toFile());
                 }
             }
             FileUtils.deleteDirectory(uuidDirectory);
-            return destiny.toPath();
+            return destiny;
         } else {
             throw new RuntimeException("Algum dos arquivos não existe");
         }
