eeae45d1156e333f43614a2a54da049d92fd9d48
giovanniandradecertacon <giovanni.andrade@certacon.com.br>
2023-05-18 09:55:46 -0300

    fix: variable corrections

--- src/main/java/br/com/certacon/certabotorganizefiles/controller/FilesController.java
+++ src/main/java/br/com/certacon/certabotorganizefiles/controller/FilesController.java
@@ -19,7 +19,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/files")
+@RequestMapping("files")
 public class FilesController {
     private final FilesService filesService;
 
