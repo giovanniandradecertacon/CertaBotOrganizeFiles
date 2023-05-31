2619f4b435eb97d33d098eddee08f40eae40e13a
giovanniandradecertacon <giovanni.andrade@certacon.com.br>
2023-05-31 17:25:48 -0300

    feat: created VO

---
+++ src/main/java/br/com/certacon/certabotorganizefiles/vo/ArquivoNFeVO.java
@@ -0,0 +1,13 @@
+package br.com.certacon.certabotorganizefiles.vo;
+
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class ArquivoNFeVO {
+    private String fileName;
+}
