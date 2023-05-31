568f81cebff3075e222121c0ecaf5d1c13e2d9a5
giovanniandradecertacon <giovanni.andrade@certacon.com.br>
2023-05-31 17:31:24 -0300

    feat: created VO for Rest

---
+++ src/main/java/br/com/certacon/certabotorganizefiles/vo/ProcessFileNFeModelVO.java
@@ -0,0 +1,36 @@
+package br.com.certacon.certabotorganizefiles.vo;
+
+import br.com.certacon.certabotorganizefiles.utils.FileStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.UUID;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class ProcessFileNFeModelVO {
+
+    private UUID id;
+    private String process;
+    private String remoteDriverDownload;
+    private String remoteDriverUpload;
+    private String taskName;
+    private String Registers;
+    private String time;
+    private String percentage;
+    private String downloadPath;
+    private String username;
+    private String password;
+    private String filePath;
+    private FileStatus status;
+    private String fileName;
+    private Date createdAt;
+    private Date updatedAt;
+    private String nomeEmpresa;
+    private String cnpj;
+}
---
+++ src/main/java/br/com/certacon/certabotorganizefiles/vo/ProcessNfeFileVO.java
@@ -0,0 +1,23 @@
+package br.com.certacon.certabotorganizefiles.vo;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class ProcessNfeFileVO {
+
+    private String url_de_download;
+    private String senha;
+    private String url_de_upload;
+    private String caminho_de_arquivo;
+    private String usuario;
+    private String caminho_de_destino_download;
+    private String nome_arquivo;
+    private String id_arquivo;
+    private String cnpj;
+    private String nome_empresa;
+
+}
