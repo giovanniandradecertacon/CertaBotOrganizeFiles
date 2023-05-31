5a31f19182b27db1a90bd96cf8035a578807ab18
giovanniandradecertacon <giovanni.andrade@certacon.com.br>
2023-05-31 17:26:42 -0300

    chore: created method for schedule control

--- src/main/java/br/com/certacon/certabotorganizefiles/repository/FilesRepository.java
+++ src/main/java/br/com/certacon/certabotorganizefiles/repository/FilesRepository.java
@@ -1,13 +1,17 @@
 package br.com.certacon.certabotorganizefiles.repository;
 
 import br.com.certacon.certabotorganizefiles.entity.FilesEntity;
+import br.com.certacon.certabotorganizefiles.utils.FileStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface FilesRepository extends JpaRepository<FilesEntity, UUID> {
     Optional<FilesEntity> findByFileName(String fileName);
+
+    List<FilesEntity> findAllByStatusEquals(FileStatus fileStatus);
 }
